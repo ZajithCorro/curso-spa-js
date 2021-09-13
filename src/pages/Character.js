7c7b0e080ea8c74fe559d2b5ae5a12a7239697bd
Zajith Corro <zajithcorrov@hotmail.com>
2021-09-13 00:06:05 -0500

    :sparkles: Rendering characters and styles

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,19 +1,37 @@
-const Character = () => {
+import getHash from '@utils/getHash'
+import getData from '@utils/getData'
+
+const Character = async () => {
+  const idHash = getHash();
+  const infoCharacter = await getData(idHash)
+
+  const { image, 
+  name,  
+  episode,
+  status,
+  species,
+  gender,
+  origin, 
+  location } = infoCharacter;
+
   const view = `
     <div class="Characters-inner">
-      <article class="Character-card">
-        <img src="image" alt"name">
-        <h2>Name</h2>
+      <article class="Characters-card">
+        <img src="${image}" alt="${name}">
+        <h2>${name}</h2>
       </article>
       <article class="Character-card">
-        <h3>Epsiodes:</h3>
-        <h3>Status:</h3>
-        <h3>Species:</h3>
-        <h3>Gender</h3>
-        <h3>Origin:</h3>
-        <h3>Last Location:</h3>
-      </article>
+        <h3>Episodes: ${episode.length}</h3>
+        <h3>Status: ${status}</h3>
+        <h3>Species: ${species}</h3>
+        <h3>Gender: ${gender}</h3>
+        <h3>Origin: ${origin.name}</h3>
+        <h3>Last Location: ${location.name}</h3>
+      </article
     </div>
   `;
+
   return view;
 };
+
+export default Character;
